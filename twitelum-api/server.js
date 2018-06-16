47948a2fb7dd79191d7691f53534cc5b5fb78327
Gustavo Lopes <p000gulopes@prservicos.com.br>
2018-06-15 22:59:17 -0300

    Altera servidor para ouvir na porta 3002

--- twitelum-api/server.js
+++ twitelum-api/server.js
@@ -1,15 +1,14 @@
-const app = require('./config/app')
-const http = require('http')
+const app = require('./config/app');
+const http = require('http');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port, function() {
     console.log(`Servidor subiu com sucesso!
     para derrubá-lo aperte Ctrl + C
     para visualizar acesse: http://localhost:${port}`)
-})
-
+});
 
 setInterval(() => {
     http.get('http://twitelum-api.herokuapp.com/')
-}, 240000)
+}, 240000);
